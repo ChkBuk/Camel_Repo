1f97191e27eeaa9c9ce49bd666529e7bad934bc7
Charith <cbkulawardana@icloud.com>
2022-05-11 13:50:40 +0800

    Initial commit

---
+++ camel-microservices-a/src/test/java/com/camel/micro/camelmicroservicesa/CamelMicroservicesAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camel.micro.camelmicroservicesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMicroservicesAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
